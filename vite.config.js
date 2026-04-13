6fbbb1f7527617941f59be05ee40c86f120a12a0
ashwinmazumdarnoob <hithisisashwinmazumdar@gmail.com>
2026-04-13 23:11:31 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    chunkSizeWarningLimit: 600,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          charts: ['recharts'],
+          parsing: ['papaparse', 'xlsx'],
+        },
+      },
+    },
+  },
+});
